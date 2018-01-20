061e9ba4c76f9ef0a8631ce3e27d33f8948068f1
Chris McDonald <963752+CMcDonald82@users.noreply.github.com>
2018-01-20 15:16:58 -0500

    add models for Symptom and Diagnosis in api app

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Symptom(models.Model):
+    name = models.CharField(max_length=256, blank=False)
+
+    def __unicode__(self):
+        return '%s' % (self.name)
+
+class Diagnosis(models.Model):
+    name = models.CharField(max_length=256, blank=False)
+    frequency = models.IntegerField(default=0, db_index=True)
+    symptom = models.ForeignKey(Symptom, related_name='diagnoses', on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ('-frequency',)
+
+    def __unicode__(self):
+        return '%s' % (self.name)
