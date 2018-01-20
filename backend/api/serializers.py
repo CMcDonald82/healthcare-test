ee73e2227234c4d99377d18f5b42ae0e221643df
Chris McDonald <963752+CMcDonald82@users.noreply.github.com>
2018-01-20 15:49:35 -0500

    add serializers for api

---
+++ backend/api/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from diagnosis.models import Symptom, Diagnosis
+
+
+class DiagnosisSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Diagnosis
+        fields = '__all__'
+
+        
+class SymptomSerializer(serializers.ModelSerializer):
+    diagnoses = DiagnosisSerializer(many=True, read_only=True)
+    class Meta:
+        model = Symptom
+        fields = '__all__'
+        depth: 2
+        
+
+class SymptomListSerializer(serializers.ModelSerializer):
+    label = serializers.CharField(source='name')
+    value = serializers.IntegerField(source='id')
+    class Meta:
+        model = Symptom
+        fields = ('label', 'value')
