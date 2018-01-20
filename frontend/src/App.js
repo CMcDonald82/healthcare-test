2669fd9edca1cc487efff125cb1d64ba3d9b2c95
Chris McDonald <963752+CMcDonald82@users.noreply.github.com>
2018-01-20 15:54:25 -0500

    start modifying App.js on frontend to handle routing with react-router (v4)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import WebFontLoader from 'webfontloader';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { Button } from 'react-md';
+import Step1Container from './containers/Step1Container';
 import './App.css';
+
+
+WebFontLoader.load({
+  google: {
+    families: ['Roboto:300,400,500,700', 'Material Icons'],
+  },
+});
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome To The Diagnosis App</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <BrowserRouter>
+          <div>
+            <Route exact path="/" component={Step1Container} />
+            
+          </div>
+        </BrowserRouter>
+
       </div>
     );
   }
